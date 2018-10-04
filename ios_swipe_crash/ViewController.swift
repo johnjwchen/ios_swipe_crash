5cf0a0649599f28aaef19c9dc3be964fdb9d6f8d
Jiawei Chen <jwchen@yelp.com>
2018-10-04 16:24:30 -0700

    done

--- ios_swipe_crash/ViewController.swift
+++ ios_swipe_crash/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MyViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var num = 20
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return num
     }
 
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return indexPath.row < 2 ? UITableViewCell() : tableView.dequeueReusableCell(withIdentifier: "SwipeRow") ?? UITableViewCell()
+    }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .normal, title: "delete") { (action, view, isSuccess) in
+            isSuccess(true)
+        }
+        action.backgroundColor = .red
+        
+        do {
+            // While the app recognie the swipe action, we many some how update the datasource, and force the table to reloadData
+            // then we'll see the crash
+            num = 2
+            tableView.reloadData()
+        }
+        
+        return UISwipeActionsConfiguration(actions: [action])
+    }
 }
 
