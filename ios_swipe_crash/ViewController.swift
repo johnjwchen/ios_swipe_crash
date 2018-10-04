c7422c451e94c89f31b270caf7b3521558dc1f24
Jiawei Chen <jwchen@yelp.com>
2018-10-04 14:48:00 -0700

    sample xcproject

---
+++ ios_swipe_crash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_swipe_crash
+//
+//  Created by Jiawei Chen on 10/4/18.
+//  Copyright © 2018 Jiawei Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
